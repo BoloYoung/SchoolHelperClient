0395dcf02929dd4922770b2bc095d392bf3549bc
BoloYoung <yuanyonghao@entekiMac.local>
2014-06-17 02:58:44 +0800

    创建项目

---
+++ SchoolHelper/SchoolHelper/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  SchoolHelper
+//
+//  Created by 袁 永浩 on 14-6-17.
+//  Copyright (c) 2014年 Horace. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SchoolHelper/SchoolHelperTests/SchoolHelperTests.m
@@ -0,0 +1,34 @@
+//
+//  SchoolHelperTests.m
+//  SchoolHelperTests
+//
+//  Created by 袁 永浩 on 14-6-17.
+//  Copyright (c) 2014年 Horace. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SchoolHelperTests : XCTestCase
+
+@end
+
+@implementation SchoolHelperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
